fd42101506764d441162b643becc8b24131c5185
Cris <crisvera1909@gmail.com>
2024-11-25 08:26:29 -0500

    quitando java (porfin)

--- back_restauranteyuli/src/main/java/com/back_restauranteyuli/back_restauranteyuli/repository/StatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.back_restauranteyuli.back_restauranteyuli.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.back_restauranteyuli.back_restauranteyuli.model.status;
-
-public interface StatusRepository extends JpaRepository<status, Integer> {
-
-}
